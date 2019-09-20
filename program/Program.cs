557cc036a747a68d9064fb0015199bd79d6d6e1f
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 15:15:18 +0200

    bekeres, max

--- program/Program.cs
+++ program/Program.cs
@@ -10,6 +10,22 @@ namespace program
     {
         static void Main(string[] args)
         {
+            int[] szamok = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Adja meg a(z) " + (i + 1) + ". számot: ");
+                szamok[i] = int.Parse(Console.ReadLine());
+            }
+            int max = szamok[0];
+            for (int i = 1; i < szamok.Length; i++)
+            {
+                if(szamok[i]>max)
+                {
+                    max = szamok[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám: " + max);
+            Console.ReadKey();
         }
     }
 }
